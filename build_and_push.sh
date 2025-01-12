2221d8f1aafe1c716f7608d973c8add26d8910ef
Juri Tomak <juritomak@Mac.fritz.box>
2025-01-12 20:33:10 +0100

    fix: explicitly build for linux/amd64 instead of using the build host's architecture

--- build_and_push.sh
+++ build_and_push.sh
@@ -3,7 +3,7 @@
 cd $1
 doctl registry login
 cleaned_path=${1%/}
-docker build -t registry.digitalocean.com/k8s-experiments-registry/experiments:$cleaned_path . && docker push registry.digitalocean.com/k8s-experiments-registry/experiments:$cleaned_path
+docker buildx build --platform linux/amd64 -t registry.digitalocean.com/k8s-experiments-registry/experiments:$cleaned_path . && docker push registry.digitalocean.com/k8s-experiments-registry/experiments:$cleaned_path
 
 doctl registry logout
 cd ../
