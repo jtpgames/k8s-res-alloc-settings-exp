3f458a9fb27f99fee05467ca06e820c1be7800a5
dez <Denny.Zerbe@gselectronic.com>
2025-09-05 14:49:04 +0200

    Kubernetes VErsion angehoben da DOCN 1.30.5 nicht länger unterstützt

--- create_k8s_cluster.sh
+++ create_k8s_cluster.sh
@@ -25,7 +25,7 @@ done
 
 echo "Creating kubernetes cluster"
 
-doctl kubernetes cluster create k8s-experiments-cluster --region fra1 --version 1.30.5-do.5 --count 2 --size s-2vcpu-4gb --verbose
+doctl kubernetes cluster create k8s-experiments-cluster --region fra1 --version 1.33.1-do.3 --count 2 --size s-2vcpu-4gb --verbose
 
 echo "Kubernetes cluster created, copying kubeconfig to terraform folder"
 
