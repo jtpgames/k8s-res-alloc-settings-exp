b6b9165bb599788d68661924b181e05f0990eb26
dez <Denny.Zerbe@gselectronic.com>
2025-09-23 11:58:35 +0200

    Memory-Allocator For_Each mit List of Strings versehen

--- terraform_teastore/modules/noise-neighbor/variables.tf
+++ terraform_teastore/modules/noise-neighbor/variables.tf
@@ -1,6 +1,8 @@
 locals {
   namespace = "noisy-neighbor"
   deployment_id = formatdate("YYYYMMDD-HHmm", timestamp())  # e.g., "20250920-1430"
+
+  memory_allocator_string_list = [for number in range(var.provisioning_memory_allocator) : tostring(number)]
 }
 
 variable "node" {
