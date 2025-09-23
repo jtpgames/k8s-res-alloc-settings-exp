b193b5e5c81df0522207cdd13e9d9e3d7c28b7f2
Juri Tomak <juritomak@Mac.fritz.box>
2025-09-23 10:45:41 +0200

    feat(Automations): build and push TeaStore.

--- build_and_push.sh
+++ build_and_push.sh
@@ -2,7 +2,7 @@
 
 cd $1
 doctl registry login
-cleaned_path=${1%/}
+cleaned_path=$(basename "${1%/}")
 docker buildx build --platform linux/amd64 -t registry.digitalocean.com/k8s-experiments-registry/experiments:$cleaned_path . && docker push registry.digitalocean.com/k8s-experiments-registry/experiments:$cleaned_path
 
 doctl registry logout
--- build_testsystem.sh
+++ build_testsystem.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
+set -e  # Exit on any error
+
 ./build_and_push.sh memory-allocator
 ./build_and_push.sh cpu-load-generator
+
+# Build TeaStore using Docker Maven builder
+echo "Building TeaStore with Maven..."
+
+docker run --rm \
+    -v "$(pwd)/TeaStore":/usr/src/app \
+    -w /usr/src/app \
+    maven:3.8.4-openjdk-11 \
+    mvn clean install -DskipTests > TeaStore/maven-build.log 2>&1
+
+echo "Maven build completed! Output saved to TeaStore/maven-build.log"
+
+./build_and_push.sh TeaStore/utilities/tools.descartes.teastore.kieker.rabbitmq
